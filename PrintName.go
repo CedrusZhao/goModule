690c09e1ed2206dc8e422525809f60440068acbe
CedrusZhao <1303806152@qq.com>
2020-11-13 16:43:30 +0800

    Update PrintName.go

--- PrintName.go
+++ PrintName.go
@@ -1,7 +1,33 @@
-package MyFirst
+package myFirst
 
 import "fmt"
 
 func MyName() {
 	fmt.Print("zhao")
+}
+
+type people1 struct {
+	name string
+	age  int
+}
+
+type People2 struct {
+	name string
+	age  int
+}
+
+func learn1() {
+	fmt.Println("learn1")
+}
+
+func Learn2() {
+	fmt.Println("learn2")
+}
+
+type read1 interface {
+	read1()
+}
+
+type Read2 interface {
+	Read2()
 }
